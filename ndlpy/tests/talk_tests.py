2000888abac41109a7293bf3e9e122b6063636cf
Neil Lawrence <lawrennd@gmail.com>
2021-05-30 14:02:45 +0100

    Fix missing quotes

--- ndlpy/tests/talk_tests.py
+++ ndlpy/tests/talk_tests.py
@@ -1,5 +1,11 @@
 from nose.tools import eq_, ok_, raises
 
+import yaml
+import os
+import re
+import sys
+import filecmp
+import unittest
 
 filename = os.path.join("_config.yml")
 testdict = {'cat': 'dog',
@@ -10,12 +16,6 @@ with open(filename, 'w') as file:
     yaml.dump(testdict, file)
 
 import ndlpy.talk as nt
-import yaml
-import os
-import re
-import sys
-import filecmp
-import unittest
 
     
 
