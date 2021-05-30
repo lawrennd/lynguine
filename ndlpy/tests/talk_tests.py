5397778f077195edbf39cb02be7853c334f09328
Neil Lawrence <lawrennd@gmail.com>
2021-05-30 14:13:01 +0100

    Set up first test

--- ndlpy/tests/talk_tests.py
+++ ndlpy/tests/talk_tests.py
@@ -7,13 +7,6 @@ import sys
 import filecmp
 import unittest
 
-filename = os.path.join(os.getcwd(), "_config.yml")
-testdict = {'cat': 'dog',
-            'butcher': 'baker',
-            'candlestick': 'maker'}
-
-with open(filename, 'w') as file:
-    yaml.dump(testdict, file)
 
 import ndlpy.talk as nt
 
@@ -22,6 +15,13 @@ import ndlpy.talk as nt
 class TalkTests(unittest.TestCase):
     def test_talk_field(self):
         """talk_tests: Test the talk_field"""
-
+        
+        filename = "my_yaml_file.yml"
+        testdict = {'cat': 'dog',
+                    'butcher': 'baker',
+                    'candlestick': 'maker'}
+        
+        with open(filename, 'w') as file:
+            yaml.dump(testdict, file)
         for key, item in testdict.items():
             self.assertTrue(nt.talk_field(key, filename)==item)
