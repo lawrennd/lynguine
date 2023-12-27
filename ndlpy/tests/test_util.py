1162118d3ff1a2c894a62b6afc0e2e1e59d9a4f7
Neil Lawrence <lawrennd@gmail.com>
2023-12-27 09:23:08 +0000

    Add CG set of tests for util and documentation.

---
+++ ndlpy/tests/test_util.py
@@ -0,0 +1,201 @@
+import pytest
+import pandas as pd
+import os
+from datetime import datetime
+from ndlpy.util import (
+    extract_full_filename, extract_file_type, extract_abs_filename, camel_capitalize,
+    remove_nan, to_valid_var, to_camel_case, sub_path_environment, get_path_env,
+    get_url_file, convert_datetime, convert_int, convert_string, convert_year_iso,
+    addmonth, addyear, augmentmonth, augmentyear, augmentcurrency, fillna, ascending,
+    descending, recent, current, former, onbool, columnis, columncontains
+)
+
+# Sample data for testing
+sample_details = {
+    "directory": "/path/to/dir",
+    "filename": "file.txt"
+}
+
+# Testing extract_full_filename
+def test_extract_full_filename():
+    expected = "/path/to/dir/file.txt"
+    assert extract_full_filename(sample_details) == expected
+
+# Testing extract_file_type
+@pytest.mark.parametrize("filename, expected", [
+    ("file.md", "markdown"),
+    ("data.csv", "csv"),
+    ("report.xlsx", "excel"),
+    ("config.yaml", "yaml"),
+    ("reference.bib", "bibtex"),
+    ("document.docx", "docx"),
+    ("unknown.file", pytest.raises(ValueError))
+])
+def test_extract_file_type(filename, expected):
+    if isinstance(expected, type) and issubclass(expected, BaseException):
+        with expected:
+            extract_file_type(filename)
+    else:
+        assert extract_file_type(filename) == expected
+
+# Testing extract_abs_filename
+def test_extract_abs_filename():
+    abs_path = os.path.abspath("/path/to/dir/file.txt")
+    assert extract_abs_filename(sample_details) == abs_path
+
+# Testing camel_capitalize
+@pytest.mark.parametrize("input, expected", [
+    ("text", "Text"),
+    ("TEXT", "TEXT"),
+    ("tExt", "Text")
+])
+def test_camel_capitalize(input, expected):
+    assert camel_capitalize(input) == expected
+
+# Testing remove_nan
+def test_remove_nan():
+    input_dict = {"a": None, "b": float('nan'), "c": 1, "d": {"da": None, "db": 2}}
+    expected_dict = {"c": 1, "d": {"db": 2}}
+    assert remove_nan(input_dict) == expected_dict
+
+# Testing to_valid_var
+@pytest.mark.parametrize("variable, expected", [
+    ("Invalid Variable", "invalid_variable"),
+    ("123numeric", "_123numeric"),
+    ("with-hyphen", "with_hyphen")
+])
+def test_to_valid_var(variable, expected):
+    assert to_valid_var(variable) == expected
+
+# Testing to_camel_case
+@pytest.mark.parametrize("text, expected", [
+    ("hello world", "helloWorld"),
+    ("HELLO WORLD", "HELLO WORLD"),
+    ("CamelCase", "camelCase"),
+    ("with/slash", "withOrSlash"),
+    ("with@symbol", "withAtSymbol"),
+    ("with-hyphen", "withHyphen"),
+    ("123number", "123number"),
+    ("", "A")
+])
+def test_to_camel_case(text, expected):
+    assert to_camel_case(text) == expected
+
+# Testing sub_path_environment
+def test_sub_path_environment():
+    # This test may vary depending on the environment it's run in
+    path = "/home/user/path"
+    env_var = "HOME"
+    expected = path.replace(os.environ.get(env_var, ''), f"${env_var}")
+    assert sub_path_environment(path) == expected
+
+# Testing get_path_env
+def test_get_path_env():
+    # Similar to sub_path_environment, this is environment dependent
+    current_path = os.getcwd()
+    expected = sub_path_environment(current_path)
+    assert get_path_env() == expected
+
+# Testing convert_datetime
+def test_convert_datetime():
+    df = pd.DataFrame({"date": ["2020-01-01", "2020-01-02"]})
+    df = convert_datetime(df, "date")
+    assert pd.api.types.is_datetime64_any_dtype(df["date"])
+
+# Testing convert_int
+def test_convert_int():
+    df = pd.DataFrame({"number": ["1", "2", None]})
+    df = convert_int(df, "number")
+    assert pd.api.types.is_integer_dtype(df["number"])
+    assert df["number"].isna().sum() == 1
+
+# Testing convert_string
+def test_convert_string():
+    df = pd.DataFrame({"text": [123, None]})
+    df = convert_string(df, "text")
+    assert all(isinstance(x, str) or pd.isna(x) for x in df["text"])
+
+# Testing convert_year_iso
+def test_convert_year_iso():
+    df = pd.DataFrame({"year": [2000, "2001", datetime(2002, 1, 1)]})
+    df = convert_year_iso(df)
+    assert all(isinstance(x, datetime) for x in df["year"])
+
+# Testing addmonth and addyear
+def test_add_month_year():
+    df = pd.DataFrame({"date": [datetime(2020, 1, 1), datetime(2020, 2, 1)]})
+    df['month'] = addmonth(df, "date")
+    df['year'] = addyear(df, "date")
+    assert df['month'].tolist() == ['January', 'February']
+    assert df['year'].tolist() == [2020, 2020]
+
+# Testing augmentmonth and augmentyear
+def test_augment_month_year():
+    df = pd.DataFrame({"date": [datetime(2020, 1, 1), None], "month": [None, "February"], "year": [None, 2021]})
+    df['month'] = augmentmonth(df, "month", "date")
+    df['year'] = augmentyear(df, "year", "date")
+    assert df['month'].tolist() == ['January', 'February']
+    assert df['year'].tolist() == [2020, 2021]
+
+# Testing augmentcurrency
+def test_augmentcurrency():
+    df = pd.DataFrame({"amount": [1234.56, 7890.123]})
+    df['formatted'] = augmentcurrency(df, "amount", 2)
+    assert df['formatted'].tolist() == ['1,234.56', '7,890.12']
+
+# Testing fillna
+def test_fillna():
+    df = pd.DataFrame({"col": [None, 1, 2]})
+    df['col'] = fillna(df, "col", 0)
+    assert df['col'].tolist() == [0, 1, 2]
+
+# Testing ascending and descending
+def test_sorting():
+    df = pd.DataFrame({"number": [3, 1, 2]})
+    df_asc = ascending(df, "number")
+    df_desc = descending(df, "number")
+    assert df_asc['number'].tolist() == [1, 2, 3]
+    assert df_desc['number'].tolist() == [3, 2, 1]
+
+# Testing recent
+def test_recent():
+    df = pd.DataFrame({"year": [2000, 2020]})
+    current_year = datetime.now().year
+    df_recent = recent(df)
+    assert df_recent.tolist() == [False, current_year == 2020]
+
+# Testing current
+def test_current():
+    now = datetime.now()
+    df = pd.DataFrame({"start": [now, now], "end": [now, now + pd.DateOffset(years=1)]})
+    df_current = current(df)
+    assert df_current.tolist() == [True, True]
+
+# Testing former
+def test_former():
+    now = datetime.now()
+    df = pd.DataFrame({"end": [now - pd.DateOffset(years=1), now]})
+    df_former = former(df)
+    assert df_former.tolist() == [True, False]
+
+# Testing onbool
+def test_onbool():
+    df = pd.DataFrame({"flag": [True, False, True]})
+    df_true = onbool(df, "flag")
+    df_false = onbool(df, "flag", invert=True)
+    assert df_true.tolist() == [True, False, True]
+    assert df_false.tolist() == [False, True, False]
+
+# Testing columnis
+def test_columnis():
+    df = pd.DataFrame({"col": ['a', 'b', 'c']})
+    df_a = columnis(df, "col", 'a')
+    assert df_a.tolist() == [True, False, False]
+
+# Testing columncontains
+def test_columncontains():
+    df = pd.DataFrame({"col": [['a', 'b'], ['b', 'c'], ['c'], 'a', 'd', None]})
+    df_contains_a = columncontains(df, "col", 'a')
+    df_contains_b = columncontains(df, "col", 'b')
+    assert df_contains_a.tolist() == [True, False, False, True, False, False]
+    assert df_contains_b.tolist() == [True, True, False, False, False, False]
--- ndlpy/util.py
+++ ndlpy/util.py
@@ -30,7 +30,14 @@ log = Logger(
 
 """Utility functions for helping, e.g. to create the relevant yaml files quickly."""
 def extract_full_filename(details):
-    """Return the filename from the details of directory and filename"""
+    """
+    Return the filename from the details of directory and filename
+
+    :param details: The details of the file to be extracted.
+    :type details: dict
+    :return: The filename.
+    :rtype: str
+    """
     if "directory" not in details or details["directory"] is None:
         return details["filename"]
     return os.path.join(
@@ -39,7 +46,14 @@ def extract_full_filename(details):
     )
 
 def extract_file_type(filename):
-    """Return a standardised file type."""
+    """
+    Return a standardised file type.
+
+    :param filename: The filename to be extracted.
+    :type filename: str
+    :return: The standardised file type.
+    :rtype: str
+    """
     ext = os.path.splitext(filename)[1][1:]
     if ext in ["md", "mmd", "markdown", "html"]:
         return "markdown" 
@@ -53,11 +67,19 @@ def extract_file_type(filename):
         return "bibtex"
     if ext in ["docx"]:
         return "docx"
-    raise ValueError(f"Unrecognised type of file in \"\{filename}\"")
+    raise ValueError(f"Unrecognised type of file in \"{filename}\"")
 
 
 def extract_abs_filename(details):
-    """Return the absolute filename by adding current directory if it's not present"""
+    """
+    Return the absolute filename by adding current directory if it's not present
+
+    :param details: The details of the file to be extracted.
+    :type details: dict
+    :return: The absolute filename.
+    :rtype: str
+    
+    """
     return os.path.abspath(extract_full_filename(details))
 
 def camel_capitalize(text):
@@ -94,7 +116,7 @@ def to_valid_var(variable):
     :type variable: str
     :return: The variable name converted to a valid variable name.
     """
-    return re.sub('\W|^(?=\d)','_', variable.lower())
+    return re.sub(r'\W|^(?=\d)','_', variable.lower())
 
 def to_camel_case(text):
     """
@@ -194,7 +216,16 @@ def convert_datetime(df, columns):
 
 
 def convert_int(df, columns):
-    """Preprocessor to set integer type on columns."""
+    """
+    Preprocessor to set integer type on columns.
+
+    :param df: The dataframe to be converted.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param columns: The columns to be converted.
+    :type columns: list
+    :return: The converted dataframe.
+    :rtype: pandas.DataFrame
+    """
     if type(columns) is not list:
         columns = [columns]
     for column in columns:
@@ -204,7 +235,16 @@ def convert_int(df, columns):
 
 
 def convert_string(df, columns):
-    """Preprocessor to set string type on columns."""
+    """
+    Preprocessor to set string type on columns.
+
+    :param df: The dataframe to be converted.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param columns: The columns to be converted.
+    :type columns: list
+    :return: The converted dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     if type(columns) is not list:
         columns = [columns]
     for column in columns:
@@ -213,9 +253,28 @@ def convert_string(df, columns):
     return df
 
 def convert_year_iso(df, column="year", month=1, day=1):
-    """Preprocessor to set string type on columns."""
+    """
+    Preprocessor to set string type on columns.
+
+    :param df: The dataframe to be converted.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param columns: The columns to be converted.
+    :type columns: list
+    :return: The converted dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
+
     def year_to_iso(field):
-        """Convert a year field to an iso date using the provided month and day."""
+        """
+        Convert a year field to an iso date using the provided month and day.
+
+        :param field: The field to be converted.
+        :type field: int or str or datetime.date
+        :return: The converted date.
+        :rtype: datetime.date
+        :raises TypeError: If the field is not of type int or str or datetime.date
+        :raises ValueError: If the field is not a valid date
+        """
         type_field = type(field)
         if type_field is int: # Assume it is integer year
             log.debug(f"Returning \"{type_field}\" from form \"{field}\"")
@@ -242,15 +301,47 @@ def convert_year_iso(df, column="year", month=1, day=1):
 
 ## Augmentors
 def addmonth(df, source="date"):
-    """Add month column based on source date field."""
+    """
+    Add month column based on source date field.
+
+    :param df: The dataframe to be augmented.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param source: The source column to be used.
+    :type source: str
+    :return: The augmented dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :raises KeyError: If the source column is not in the dataframe
+    :raises TypeError: If the source column is not of type datetime.date
+    :raises ValueError: If the source column is not a valid date
+    """
     return df[source].apply(lambda x: x.month_name() if x is not None else pd.NA)
 
 def addyear(df, source="date"):
-    """Add year column and based on source date field."""
+    """
+    Add year column and based on source date field.
+
+    :param df: The dataframe to be augmented.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param source: The source column to be used.
+    :type source: str
+    :return: The augmented dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     return df[source].apply(lambda x: x.year if x is not None else pd.NA)
 
 def augmentmonth(df, destination='month', source="date"):
-    """Augment the  month column based on source date field."""
+    """
+    Augment the  month column based on source date field.
+
+    :param df: The dataframe to be augmented.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param destination: The destination column to be used.
+    :type destination: str
+    :param source: The source column to be used.
+    :type source: str
+    :return: The augmented dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     val = pd.Series(index=df.index)
     for index, entry in df.iterrows():
         if pd.isna(df.at[index, destination]) and not pd.isna(df.at[index, source]):
@@ -260,7 +351,18 @@ def augmentmonth(df, destination='month', source="date"):
     return val
 
 def augmentyear(df, destination="year", source="date"):
-    """Augment the year column based on source date field."""
+    """
+    Augment the year column based on source date field.
+
+    :param df: The dataframe to be augmented.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param destination: The destination column to be used.
+    :type destination: str
+    :param source: The source column to be used.
+    :type source: str
+    :return: The augmented dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     val = pd.Series(index=df.index)
     for index, entry in df.iterrows():
         if pd.isna(df.at[index, destination]) and not pd.isna(df.at[index, source]):
@@ -270,31 +372,98 @@ def augmentyear(df, destination="year", source="date"):
     return val
 
 def augmentcurrency(df, source="amount", sf=0):
-    """Preprocessor to set integer type on columns."""
+    """
+    Preprocessor to set integer type on columns.
+
+    :param df: The dataframe to be converted.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param columns: The columns to be converted.
+    :type columns: list
+    :return: The converted dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     fstr=f"{{0:,.{sf}f}}"
     return df[source].apply(lambda x: fstr.format(x))
 
 
-def addsupervisor(df, column, supervisor):
-    return df[column].fillna(supervisor)    
+def fillna(df, column, value):
+    """
+    Fill missing values in a column with a given value.
+
+    :param df: The dataframe to be converted.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param column: The column to be converted.
+    :type column: str
+    :param value: The value to be used to fill missing values.
+    :type value: str
+    :return: The converted dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
+    return df[column].fillna(value)
 
 ## Sorters
 def ascending(df, by):
-    """Sort in ascending order"""
+    """
+    Sort in ascending order
+
+    :param df: The dataframe to be sorted.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param by: The column to be sorted by.
+    :type by: str
+    :return: The sorted dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     return df.sort_values(by=by, ascending=True)
 
 def descending(df, by):
-    """Sort in descending order"""
+    """
+    Sort in descending order
+
+    :param df: The dataframe to be sorted.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param by: The column to be sorted by.
+    :type by: str
+    :return: The sorted dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     return df.sort_values(by=by, ascending=False)
 
 ## Filters
 def recent(df, column="year"):
-    """Filter on year of item"""
+    """
+    Filter on whether item is recent
+
+    :param df: The dataframe to be filtered.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param column: The column to be filtered on.
+    :type column: str
+    :return: The filtered dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    
+    """
     return df[column]>=get_since_year()
 
-def current(df, start="start", end="end", current=None):
-    """Filter on whether item is current"""
-    now = pd.to_datetime(datetime.datetime.now().date())
+def current(df, start="start", end="end", current=None, today=None):
+    """
+    Filter on whether the row is current as given by start and end dates. If current is given then it is used instead of the range check. If today is given then it is used instead of the current date.
+    
+    :param df: The dataframe to be filtered.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param start: The start date of the entry.
+    :type start: str
+    :param end: The end date of the entry.
+    :type end: str
+    :param current: Column of true/false current entries.
+    :type current: str
+    :param today: The date to be used as today.
+    :type today: datetime.date
+    :return: The filtered dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
+    if today is None:
+        now = pd.to_datetime(datetime.datetime.now().date())
+    else:
+        now = today
     within = ((df[start] <= now) & (pd.isna(df[end]) | (df[end] >= now)))
     if current is not None:
         return (within | (~df[current].isna() & df[current]))
@@ -302,22 +471,63 @@ def current(df, start="start", end="end", current=None):
         return within
 
 def former(df, end="end"):
-    """Filter on whether item is current"""
+    """
+    Filter on whether item is former.
+
+    :param df: The dataframe to be filtered.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param end: The end date of the entry.
+    :type end: str
+    :return: The filtered dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     now = pd.to_datetime(datetime.datetime.now().date())
     return (df[end] < now)
 
 def onbool(df, column="current", invert=False):
-    """Filter on whether column is positive (or negative if inverted)"""
+    """
+    Filter on whether column is positive (or negative if inverted)
+
+    :param df: The dataframe to be filtered.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param column: The column to be filtered on.
+    :type column: str
+    :param invert: Whether to invert the filter.
+    :type invert: bool
+    :return: The filtered dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     if invert:
         return ~df[column]
     else:
         return df[column]
 
 def columnis(df, column, value):
-    """Filter on whether item is equal to a given value"""
+    """
+    Filter on whether a given column is equal to a given value
+
+    :param df: The dataframe to be filtered.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param column: The column to be filtered on.
+    :type column: str
+    :param value: The value to be used to filter.
+    :return: The filtered dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
+    
     return (df[column]==value)
 
 def columncontains(df, column, value):
-    """Filter on whether column contains a given value"""
+    """
+    Filter on whether column contains a given value
+
+    :param df: The dataframe to be filtered.
+    :type df: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    :param column: The column to be filtered on.
+    :type column: str
+    :param value: The value to be used to filter.
+    :return: The filtered dataframe.
+    :rtype: pandas.DataFrame or ndlpy.data.CustomDataFrame
+    """
     colis = columnis(df, column, value)
     return (colis | df[column].apply(lambda x: (x==value).any() if type(x==value) is not bool else (x==value)))
